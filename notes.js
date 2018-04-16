471e4b4a6cdb8604a79548e5c15116836e510231
Sajidansari <lanetteam.sajid@gmail.com>
2018-04-16 16:05:06 +0530

    Note App using command line YARGS

--- notes.js
+++ notes.js
@@ -1,27 +1,63 @@
 /**
  * Created by lcom25 on 3/4/18.
  */
-console.log('Starting notes.js !');
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try{
+        var noteString = fs.readFileSync('note-data.json');
+        return JSON.parse(noteString);
+    }
+    catch (e) {
+        return [];
+    }
+}
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('note-data.json', JSON.stringify(notes));
+}
 
 var addNote = (title, body) => {
-    console.log('Adding note ', title, body);
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+    var duplicateNotes = notes.filter((note) => note.title === title );
+    if(duplicateNotes.length === 0){
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
 };
 
 var getAll = () => {
-    console.log('Getting all notes')
+    return fetchNotes();
 };
 
 var getNotes = (title) => {
-    console.log('Reading title', title);
+    var notes = fetchNotes();
+    var foundNote = notes.filter((note) => note.title === title);
+    return foundNote[0];
 }
 
-var removeNote = () => {
-    console.log('Deleting note');
+var removeNote = (title) => {
+    var notes = fetchNotes();
+    var filteredNotes =  notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
+    return notes.length !== filteredNotes.length;
+}
+
+var logNotes = (note) => {
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 }
 
 module.exports = {
     addNote,
     getAll,
     getNotes,
-    removeNote
+    removeNote,
+    logNotes
 };
